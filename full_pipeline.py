f0d1cb2868470b3bb00561a651a7c010f9206bce
Laith Abuassoud <110863545+LaithAbuAssoud@users.noreply.github.com>
2022-12-30 21:01:27 +0300

    Update full_pipeline.py

--- full_pipeline.py
+++ full_pipeline.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np 
 #import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 import nltk 
 
 from transformers import AutoTokenizer
